c75f271ba437001500cc6f04cb05c6fd0a911611
Fiujy <56654641+Fiujy@users.noreply.github.com>
2023-07-30 13:07:42 +0200

    Plusieurs modifications d'interfaces et de fonctionnalités
    - La page de détails de produits a été améliorés : les informations inutiles ont été supprimées, certains ont changés de places pour un positionnement cohérent
    - Une appBar a été ajouté à la page de détails du produit (CartView)
    - En premier test : Le bouton plus (rose) de la pizza pimento dans la catégorie popular redirige désormais vers CartView

--- lib/screens/home/views/cart/components/cartitem/cart_item.dart
+++ lib/screens/home/views/cart/components/cartitem/cart_item.dart
@@ -5,7 +5,8 @@ import './components/build_image.dart';
 import './components/descriptions_and_ratings.dart';
 import './components/price_and_cart_actions.dart';
 import 'package:foodcourt/themes/app_colors.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+//import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:badges/badges.dart' as badges;
 
 class CartItem extends StatelessWidget {
   const CartItem({
@@ -27,54 +28,72 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          BuildImage(image: image),
-          const SizedBox(height: 10),
-          SmoothIndicator(
-            offset: index.toDouble(),
-            count: 4,
-            effect: const WormEffect(
-              activeDotColor: Colors.pink,
-              spacing: 12,
-            ), size: const Size(0, 0),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Test'),
+          actions: <Widget>[
+            badges.Badge(
+              //position: badges.BadgePosition.topEnd(top: -10, end: -5),
+              showBadge: false,
+              ignorePointer: false,
+              onTap: () {},
+              child: IconButton(
+                icon: const Icon(Icons.shopping_cart_outlined),
+                onPressed: () {},
+              )
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              BuildImage(image: image),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+              /*SmoothIndicator(
+                offset: index.toDouble(),
+                count: 4,
+                effect: const WormEffect(
+                  activeDotColor: Colors.pink,
+                  spacing: 12,
+                ),
+                size: const Size(0, 0),
+              ),*/
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.w),
+                child: Column(
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.001),
+                    BuildDescriptionsAndRatings(
+                      title: title,
+                      desc: desc,
+                      //rating: rating,
+                      index: index,
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                    const AddYourMeal(),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                    BuildPriceAndCartActions(
+                      price: price,
+                      index: index,
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        minimumSize: const Size(double.infinity, 60),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Place Order',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                    //SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.w),
-            child: Column(
-              children: [
-                BuildPriceAndCartActions(
-                  price: price,
-                  index: index,
-                ),
-                SizedBox(height: 34.h),
-                BuildDescriptionsAndRatings(
-                  title: title,
-                  desc: desc,
-                  rating: rating,
-                  index: index,
-                ),
-                SizedBox(height: 25.h),
-                const AddYourMeal(),
-                SizedBox(height: 35.h),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: primaryColor,
-                    minimumSize: const Size(double.infinity, 60),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'Place Order',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ),
-                SizedBox(height: 12.h),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
--- lib/screens/home/views/cart/components/cartitem/components/build_image.dart
+++ lib/screens/home/views/cart/components/cartitem/components/build_image.dart
@@ -15,12 +15,12 @@ class BuildImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: const BorderRadius.only(
-        bottomLeft: Radius.circular(50),
-        bottomRight: Radius.circular(50),
+        bottomLeft: Radius.circular(30),
+        bottomRight: Radius.circular(30),
       ),
       child: SizedBox(
         width: double.infinity,
-        height: Get.height * 0.4,
+        height: Get.height * 0.3,
         child: Transform.scale(
           scale: 1.1,
           child: AppFadeinImageView(
